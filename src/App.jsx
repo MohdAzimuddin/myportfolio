f17bbadee00e188e288e35ef0b5ca860e3f643fc
MohdAzimuddin <sfsuper2020@gmail.com>
2025-07-09 01:57:57 +0530

    removed unnecessary compoenent

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Tech from './components/Tech/Tech';
-import Projects from './components/Projects/Projects';
+import Projects from './components/Projectss/Projects';
 import Contact from './components/Contact/Contact';
 import './App.css'; // Import the CSS file
 import Experience from './components/Experience/Experience';
